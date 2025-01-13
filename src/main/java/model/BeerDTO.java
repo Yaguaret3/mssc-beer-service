702fcca9a72fa0278fd0c274746609ba29441dfb
Pablo Leyes <n.leyes.n@gmail.com>
2025-01-12 21:37:59 -0300

    Beer Model Created

---
+++ src/main/java/model/BeerDTO.java
@@ -0,0 +1,31 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDTO {
+
+    private Integer version;
+
+    private UUID id;
+
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private Long quantityOnHand;
+
+    private Long upc;
+    private BigDecimal price;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lasModifiedDate;
+}
---
+++ src/main/java/model/BeerStyleEnum.java
@@ -0,0 +1,6 @@
+package model;
+
+public enum BeerStyleEnum {
+
+    LAGER, PILSNER, STOUR, ALE, GOSE, PORTER, WHEAT, IPA, PALE_ALE, SAISON;
+}
