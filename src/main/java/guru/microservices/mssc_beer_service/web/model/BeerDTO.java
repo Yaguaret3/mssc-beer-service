2975809a8b44997e4d8ebc3c8f0964ebe05db92f
Pablo Leyes <n.leyes.n@gmail.com>
2025-01-23 01:26:29 -0300

    @Valid Annotations

--- src/main/java/guru/microservices/mssc_beer_service/web/model/BeerDTO.java
+++ src/main/java/guru/microservices/mssc_beer_service/web/model/BeerDTO.java
@@ -1,5 +1,9 @@
 package guru.microservices.mssc_beer_service.web.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,17 +19,27 @@ import java.util.UUID;
 @Builder
 public class BeerDTO {
 
+    @Null
     private Integer version;
 
+    @Null
     private UUID id;
 
+    @NotBlank
     private String beerName;
+    @NotNull
     private BeerStyleEnum beerStyle;
     private Long quantityOnHand;
 
+    @NotNull
+    @Positive
     private Long upc;
+    @NotNull
+    @Positive
     private BigDecimal price;
 
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lasModifiedDate;
 }
